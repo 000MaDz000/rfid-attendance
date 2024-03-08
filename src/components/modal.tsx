bc77a1cc90427964a5f94f320ae368dad8d3864a
mostafa mohammed <000madz000@gmail.com>
2024-03-09 01:16:32 +0200

    adding background prop

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,8 +1,8 @@
 import { PropsWithChildren } from "react";
 
-export default function Modal({ children }: PropsWithChildren) {
+export default function Modal({ children, background }: PropsWithChildren & { background?: string }) {
     return (
-        <div className="absolute h-full w-full bg-inherit">
+        <div className={"absolute h-full w-full " + (background ? background : "bg-inherit")}>
             <div className="relative w-full h-full">
                 <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
                     {children}
